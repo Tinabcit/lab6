b19e3465655093a657bf9739514ad8f51d0ab84b
lab 6 <tshabestari@my.bcit.ca>
2020-11-17 17:04:04 -0800

    Add Javascript file, add array course

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+var coursesList = ['name','code'];
+
+coursesList[1] = 'name: Web Fundemental Technologies','code = ACIT 1620';
+console.log(coursesList)
+coursesList: {
+    code: 'ACIT 1620',
+    name: 'Web Fundemental Technologies',
+
+    code: 'ACIT 1420',
+    name: 'Introduction to System Administration',
+
+    code: 'Math 1310',
+    name: 'Math for IT'
+}
