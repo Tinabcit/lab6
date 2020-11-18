7c74726bbdc63b79ff2db7c4dbed15a08736f343
lab 6 <tshabestari@my.bcit.ca>
2020-11-17 17:16:18 -0800

    change in while from var and add console log

--- JS/script.js
+++ JS/script.js
@@ -3,17 +3,17 @@ coursesList[1] = 'name: Web Fundemental Technologies','code = ACIT 1620';
 console.log(coursesList)
 coursesList: {
     code: 'ACIT 1620',
-    name: 'Web Fundemental Technologies',
+    name; 'Web Fundemental Technologies',
 
-    code: 'ACIT 1420',
-    name: 'Introduction to System Administration',
+    code; 'ACIT 1420',
+    name; 'Introduction to System Administration',
 
-    code: 'Math 1310',
-    name: 'Math for IT'
+    code; 'Math 1310',
+    name; 'Math for IT'
 }
-let i =0
+let i =0;
 let arraycoursesList= array. coursesList
 while ( i< arraycoursesList) {
-    let var = array[i];
+    console.log(i);
     i++;
 }
